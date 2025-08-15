c702af32e78eece794d87e7fad884ef675426d36
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-15 16:04:02 -0400

    add authenticate and signup apis

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -7,12 +7,47 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
+  const [username, setUsername] = useState();
 
-  // TODO: signup
+  const signUp = async (body) => {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const responseJSON = await response.json(); //{success: ... , message: 'Thanks for signing up!', token: '....'}
+      const responseJSONToken = responseJSON.token; //{token: '....'}
+      console.log(responseJSON);
+      setToken(responseJSONToken);
+      setLocation("TABLET"); // Move to next location, which is the TABLET location
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // TODO: authenticate
+  const authenticate = async () => {
+    try {
+      const response = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const responseJSON = await response.json(); //{success: ... , message: 'Thanks for signing up!', token: '....'}
+      const usernameFromToken = responseJSON.data.username;
+      console.log(usernameFromToken);
+      console.log(username);
+      //if username value from authenticate API and user input matches
+      // enter the tunnel by removing the setLocation state variable.
+      if (usernameFromToken === username) setLocation();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const value = { location };
+  const value = { location, signUp, setUsername, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
